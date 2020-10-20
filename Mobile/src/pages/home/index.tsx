4e30dd9b9a7bbdebd15251093b15141b15207bf1
mikey espin <mikeyespin@gmail.com>
2020-10-19 22:43:13 -0300

    criando página home

--- Mobile/src/pages/home/index.tsx
+++ Mobile/src/pages/home/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons';
+import { View, ImageBackground, Text, Image, StyleSheet } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const home = () => {
     return (
-    <View style = {styles.container}>
-        <Image source={require('../../assets/logo.png')}/>
-    </View>
+    <ImageBackground 
+      source = {require ('../../assets/home-background.png')} 
+      style = {styles.container}
+      imageStyle = {{ width: 274, height: 368 }}
+    >
+      <View style = {styles.main}>
+       <Image source={require('../../assets/logo.png')}/>
+       <Text style = {styles.title}>Seu Marketplace de coleta de resíduos</Text>
+       <Text style = {styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+
+      </View>
+
+      <View style = {styles.footer}>
+        <RectButton style = {styles.button} onPress = { () => {}}>
+          <View style = {styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size= {24} />
+            </Text>
+          </View>
+          <Text style = {styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
+      </View>
+        
+    </ImageBackground>
     );
 };
 
@@ -13,6 +38,7 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 32,
+      backgroundColor: '#f0f0f5',
     },
   
     main: {
@@ -76,6 +102,6 @@ const styles = StyleSheet.create({
       fontFamily: 'Roboto_500Medium',
       fontSize: 16,
     }
-  });
+  });  
   
 export default home;
